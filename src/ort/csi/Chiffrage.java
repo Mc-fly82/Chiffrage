715826272b027b60b13bc4e905731855795042bc
Unknown <lucaspellegrinowd@gmail.com>
2018-07-03 10:19:13 +0200

    aze

--- src/ort/csi/Chiffrage.java
+++ src/ort/csi/Chiffrage.java
@@ -2,6 +2,20 @@ package ort.csi;
 import java.math.BigInteger;
 
 public class Chiffrage {
+
+    public static int primeGen()
+    {
+        double prime = Math.random();
+        int primeNumbers[]= {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+
+        return (int)prime;
+    }
     
+    public int encryption(String msg , int t1 ) {
+
+
+
+        return result;
+    }
     
 }
